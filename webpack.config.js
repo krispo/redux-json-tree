6c2af59809864db1ad78871ade97c29be1302190
krispo <konstantin.skipor@gmail.com>
2016-02-25 14:56:25 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './index',
+  output: {
+    path: './',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3000
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css|.scss$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          'css-loader?sourceMap&modules&localIdentName=[local]!postcss-loader'
+        ),
+      }
+    ]
+  },
+  postcss: function() {
+    return [
+      require('precss')(),
+      require('autoprefixer')(),
+    ];
+  },
+  plugins: [
+    new ExtractTextPlugin("bundle.css", { allChunks: true })
+  ]
+}
