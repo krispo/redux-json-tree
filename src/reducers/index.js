4d49491a56aaa89a20591c046e77526e427a46aa
krispo <konstantin.skipor@gmail.com>
2016-04-11 09:47:13 +0300

    update reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,14 @@
 import { UPDATE } from '../actions'
-import {update, updateRaw} from 'simplifr'
 
 export function reducer(state = {}, action){
   const { path } = action
   if (typeof path === 'undefined') {
     return state
   }
-  return update(
-    Object.assign({}, state),
-    action.path,
-    isNaN(+action.value) ? action.value : +action.value
-  )
+  switch (action.type) {
+    case UPDATE:
+      return Object.assign({}, state, { [action.path]: isNaN(+action.value) ? action.value : +action.value })
+    default:
+      return state
+  }
 }
